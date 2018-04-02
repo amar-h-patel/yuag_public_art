ceb3ef1fd5501973de44c4e5d67386f93adabc6e
Jon Rolfe <jon-rolfe@users.noreply.github.com>
2018-04-02 18:38:52 -0400

    add basic database logic

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,43 @@
 var express = require('express');
+const Sequelize = require('sequelize');
+const PORT = process.env.PORT || 3000;
+const DB_PASSWORD = String(process.env.PASSWORD);
+const HOST = process.env.HOST || 'ec2-52-55-107-25.compute-1.amazonaws.com';
 var app = express();
 
-app.use(express.static(__dirname +'./../../')); //serves the index.html
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+console.log('local port: %s\ndb location: %s\ndb password: %s', PORT, HOST, DB_PASSWORD)
+
+// set up time: make an object with all the data Sequelize needs to connect
+const database = new Sequelize({
+    database: 'publicart',
+    username: 'postgres',
+    password: DB_PASSWORD,
+    host: HOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
+});
+
+// now it's time to actually attempt to connect to the db
+database
+    .authenticate()
+    .then(() => {
+        console.log('we did it, connection worky!');
+    })
+    .catch(err => {
+            console.log('connection failed :(', err)
+        }
+    );
+
+database
+    .query(
+        `SELECT COLUMN_NAME 
+        FROM information_schema.COLUMNS 
+        WHERE TABLE_NAME = 'art_details'`
+    )
+    .spread((results, metadata) => {
+        console.log(results)
+    })
+
+app.use(express.static(__dirname + './../../')); //serves the index.html
+app.listen(PORT); //listens on localhost on specified port (default 3000)
+
