aa81f4a51c154131f568702d315ff92b2927aaf6
amar-h-patel <amarpatel0702@gmail.com>
2018-04-05 16:10:23 -0400

    added DB logic

--- app.js
+++ app.js
@@ -3,6 +3,58 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const Sequelize = require('sequelize');
+const DB_PASSWORD = String(process.env.PASS) || "";
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || 'ec2-52-55-107-25.compute-1.amazonaws.com';
+
+console.log('local port: %s\ndb location: %s\ndb password: %s',
+    PORT, HOST, DB_PASSWORD);
+
+if (DB_PASSWORD == ""){
+    console.log('no password specified - terminating');
+    process.exit(1)
+}
+
+let database = new Sequelize({
+    database: 'publicart',
+    username: 'postgres',
+    password: DB_PASSWORD,
+    host: HOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
+});
+
+function open_db() {
+    // now it's time to actually attempt to connect to the db
+    database
+        .authenticate()
+        .then(() => {
+            console.log('we did it, connection worky!');
+        })
+        .catch(err => {
+                console.log('connection failed :(', err)
+            }
+        );
+}
+
+// test fn - describe the art_details table
+function db_test() {
+    database
+        .query(
+            `SELECT COLUMN_NAME 
+        FROM information_schema.COLUMNS 
+        WHERE TABLE_NAME = 'art_details'`
+        )
+        .spread((results, metadata) => {
+            console.log(results);
+            console.log('sample query successful')
+        });
+}
+
+open_db();
+db_test();
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
