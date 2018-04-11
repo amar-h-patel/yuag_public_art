7eaa25a45898dcd8b8896ad50f7779d5b3b75266
amar-h-patel <amarpatel0702@gmail.com>
2018-04-11 15:50:50 -0400

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
-
-import Jumbo from "./Views/Jumbo.js";
-import Gallery from "./Views/Gallery.js";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Headnav from "./Views/Headnav.js";
 import Viewer from "./Views/Viewer.js";
+import Maps from "./Views/Maps.js";
+import Submit from "./Views/Submit.js";
+import About from "./Views/About.js";
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header>
           
           <div> <Headnav/> </div>
-          <div> <Jumbo/> </div>
-          
-
 
         </header>
-        <p className="App-intro">
-          <div> <Gallery/> </div>
-          <div> <Viewer/> </div>
-        </p>
+         <Route path="/maps" component={Maps}/>
+         <Route path="/submit" component={Submit}/>
+         <Route path="/about" component={About}/>
+         <Route exact path="/" component={Viewer}/>
+       
       </div>
+      </Router>
     );
   }
 }
--- client/src/Views/Headnav.js
+++ client/src/Views/Headnav.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
-import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 import MenuItem from 'react-bootstrap/lib/MenuItem';
 import '../../node_modules/bootstrap3/dist/css/bootstrap.css';
 
@@ -29,25 +28,24 @@ class Headnav extends Component {
         <Navbar style={styles} inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand >
+
               <img style={imgStyle} src="https://artgallery.yale.edu/sites/all/themes/artgallery/logo.png" alt="YUAG"/>
+
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header> 
           <Navbar.Collapse>
             <Nav style={textStyle} pullRight>
-              <NavItem eventKey={1} href="#">
-                Browse
+              <NavItem eventKey={1} href="/about">
+                About
               </NavItem>
-              <NavItem eventKey={2} href="#">
+              <NavItem eventKey={2} href="/maps">
+                Map
+              </NavItem>
+              <NavItem eventKey={2} href="/submit">
                 Submit
               </NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
+
             </Nav>
           </Navbar.Collapse>
         </Navbar> );
