b9e2d651945f93ef2fd8e2f6690082f475826be5
Alexander Wisowaty <alexanderwisowaty@Alexanders-MacBook-Pro.local>
2018-04-16 15:15:38 -0400

    fixed jumbo background

--- client/src/Views/Jumbo.js
+++ client/src/Views/Jumbo.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import {Button} from "react-bootstrap";
 import {Jumbotron} from "react-bootstrap";
 import '../../node_modules/bootstrap3/dist/css/bootstrap.css';
+import './Jumbo.css';
 
 class Jumbo extends Component {
   render() {
-    var styles={ // not rendering image!
-        "background-image": "https://images.pexels.com/photos/574345/pexels-photo-574345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-        "background-size": "cover"
-    };
+
     return (
       <div className="Jumbo">
-        <Jumbotron syle={styles}>
+        <Jumbotron>
             <h1>Public Art at Yale</h1>
             <br/>
             <p>
@@ -20,10 +18,10 @@ class Jumbo extends Component {
             <br/>
             <br/>
 
-            <Button bsStyle="primary" href="#" >Learn more</Button>
+            <Button bsStyle="primary" href="/about" >Learn more</Button>
             <br/>
             <br/>
-            <Button bsStyle="primary" href="#" >Submit a Piece</Button> 
+            <Button bsStyle="primary" href="/submit" >Submit a Piece</Button> 
             {/*link these!*/}
 
         </Jumbotron>
